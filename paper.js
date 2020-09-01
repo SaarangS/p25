3cfbb7030b77adbefe32da73d45a3748a20f5214
SaarangS <69085865+SaarangS@users.noreply.github.com>
2020-08-31 19:27:07 -0500

    Update paper.js

--- paper.js
+++ paper.js
@@ -21,9 +21,9 @@ class paper {
 		push()
 		translate(paperpos.x, paperpos.y);
 		rectMode(CENTER)
-		strokeWeight(3);
-		fill(255,0,255)
-		ellipse(0,0,this.r, this.r);
+
+		imageMode(CENTER);
+		image(this.image, 0, 0, this.r, this.r);
 
 		pop()
 			
